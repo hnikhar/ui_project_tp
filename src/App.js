5024408d7e63e35c77b1a3bff6bea81ec31d458a
Harsha Nikhar <harsha.pry@gmail.com>
2024-04-14 02:42:16 -0700

    FIx body/UI structure

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import TreeMenu from './components/TreeMenu/TreeMenu';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="main-content">
-        <h2>Dynamic Tree Menu</h2>
-        <TreeMenu />
-      </div>
+      <div className="main-container">
+        <h2 className="tree-heading">Dynamic Tree Menu</h2>
+        <div className="main-content">
+          <TreeMenu />
+        </div>
+        </div>
       <Footer />
     </div>
   );
